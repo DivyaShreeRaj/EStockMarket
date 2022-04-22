2f212513f3736a28653f08d1976a96c911f78b5b
DivyaShreeRaj <104213527+DivyaShreeRaj@users.noreply.github.com>
2022-04-22 20:06:20 +0530

    Add files via upload

---
+++ stock/src/main/java/com/market/stock/controller/StockManagementController.java
@@ -0,0 +1,15 @@
+package com.market.stock.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/api/v1.0/market/stock/")
+public class StockManagementController {
+
+	/*
+	 * @PostMapping("add/{companycode}") public void addStocks() {
+	 * 
+	 * }
+	 */
+
+}
